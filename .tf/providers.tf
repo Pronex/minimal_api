1592895cef1f67a6d1e17ee3662e6d29fc443ffc
Diego Mueller <muellerjesko@gmail.com>
2023-05-18 22:38:15 +0200

    tf github action & remodeled tf

--- .tf/providers.tf
+++ .tf/providers.tf
@@ -1,16 +1,20 @@
+# configure azure provider
+provider "azurerm" {
+  features {}
+}
+
 # Define Terraform backend using a blob storage container on Microsoft Azure for storing the Terraform state
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.50"
-    }
-  }
-
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "tfstorraw"
     container_name       = "tfstate"
     key                  = "api.tfstate"
   }
+}
+
+# data sources
+data "azurerm_container_registry" "acr-minimal-api" {
+  name                = var.acr-minimal-api
+  resource_group_name = var.rg-acr
 }
